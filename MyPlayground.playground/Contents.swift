830e5f16428e404a70f1c79b05be5e5cfe9e2f89
Ashr005 <as2733@sussex.ac.uk>
2024-02-20 13:15:52 +0000

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,16 +1,17 @@
 class VirtualBankSystem {
     var accountType = ""
-    func welcomeCustomer(){
+    var isOpened = true
+    func welcomeCustomer() {
         print("Welcome to your virtual bank system.")
     }
-    func onboardCustomerAccountOpening(){
-        print("Welcome kind of account would you like to open?")
+    func onboardCustomerAccountOpening() {
+        print("What kind of account would you like to open?")
         print("1. Debit account")
         print("2. Credit account")
-}
-    func makeAccount(numberPadKey: Int){
-        print("The selected option is \(numberPadKey)")
-        switch numberPadKey{
+    }
+    func makeAccount(numberPadKey: Int) {
+        print("The selected option is \(numberPadKey).")
+        switch numberPadKey {
         case 1:
             accountType = "debit"
         case 2:
@@ -21,26 +22,64 @@ class VirtualBankSystem {
         }
         print("You have opened a \(accountType) account.")
     }
+    func moneyTransfer(transfertype: String, transferAmount: Int, bankAccount:in-out){
+        
+    }
 }
+
+struct BankAccount {
+    var debitBalance = 0
+    var creditBalance = 0
+    let creditLimit = 100
+    var debitBalanceInfo: String {
+        "Debit balance: $\(debitBalance)"
+    }
+    var availableCredit: Int {
+        creditLimit + creditBalance
+    }
+    var creditBalanceInfo: String {
+        "Available credit: $\(availableCredit)"
+    }
+    mutating func debitDeposit(_ amount: Int) {
+        debitBalance += amount
+        print("Debit deposit: $\(amount). \(debitBalanceInfo)")
+    }
+    mutating func debitWithdraw(_ amount: Int) {
+        if amount > debitBalance {
+            print("Insufficient funds to withdraw $\(amount). \(debitBalanceInfo)")
+        } else {
+            debitBalance -= amount
+            print("Debit withdraw: $\(amount). \(debitBalanceInfo)")
+        }
+    }
+    mutating func creditDeposit(_ amount: Int) {
+        creditBalance += amount
+        print("Credit deposit: $\(amount). \(creditBalanceInfo)")
+        if creditBalance == 0 {
+            print("Paid off credit balance.")
+        } else if creditBalance > 0 {
+            print("Overpaid credit balance.")
+        }
+    }
+    mutating func creditWithdraw(_ amount: Int) {
+        if amount > availableCredit {
+            print("Insufficient credit to withdraw $\(amount). \(creditBalanceInfo)")
+        } else {
+            creditBalance -= amount
+            print("Credit withdraw: $\(amount). \(creditBalanceInfo)")
+        }
+    }
+}
+
 let virtualBankSystem = VirtualBankSystem()
 virtualBankSystem.welcomeCustomer()
-repeat{
+
+repeat {
     virtualBankSystem.onboardCustomerAccountOpening()
     let numberPadKey = Int.random(in: 1...3)
     virtualBankSystem.makeAccount(numberPadKey: numberPadKey)
 } while virtualBankSystem.accountType == ""
-            struct BankAccount{
-    var debitBalance = 0
-    var creditBalance = 0
-    let creditLimit = 100
-    
-    var debitBalanceInfo: String{
-        return "Debit balance: $\(debitBalance)"
-    }
-    var availableCredit: Int{
-        return creditLimit+creditBalance
-    }
-    var creditBalanceInfo: String{
-        return "Available credit: $\(availableCredit)"
-    }
-}
+
+let transferAmount = 50
+print("Transfer amount: $\(transferAmount)")
+var bankAccount = BankAccount()
